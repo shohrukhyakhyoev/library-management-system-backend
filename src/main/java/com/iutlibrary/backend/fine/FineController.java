a20237c7dec655fd4e4d5fe4c34abcbcf71311d8
shohrukhyakhyoev <shohrukhyakhyoev@gmail.com>
2022-11-25 17:09:22 +0500

    crossorigin is removed

--- src/main/java/com/iutlibrary/backend/fine/FineController.java
+++ src/main/java/com/iutlibrary/backend/fine/FineController.java
@@ -1,7 +1,6 @@
 package com.iutlibrary.backend.fine;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
--- src/main/java/com/iutlibrary/backend/security/WebSecurityConfig.java
+++ src/main/java/com/iutlibrary/backend/security/WebSecurityConfig.java
@@ -10,10 +10,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
+@CrossOrigin(maxAge = 3600)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final AccountService accountService;
